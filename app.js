126dff02d83d6689b03e11f15c0b1897d724e2fe
Kalindu Agathisi <kalinduw@ceyentra.com>
2024-02-01 13:29:35 +0530

    exporting modules

---
+++ app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+// write to file
+// fs.writeFileSync('notes.txt', 'My name is Kali');
+
+// append to file
+// fs.appendFileSync('notes.txt', '\nThis is test append!');
+
+const name = require('./util.js');
+
+console.log(name);
---
+++ util.js
@@ -0,0 +1,5 @@
+console.log('util.js');
+
+const name = 'Alistor';
+
+module.exports = name;
